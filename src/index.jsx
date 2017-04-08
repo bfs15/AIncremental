ee331bec7c52fd917a30def23b46787168cd73e5
bfs15 <bruno.serbena@gmail.com>
2017-04-08 18:36:38 -0300

    changed eslint config

--- src/index.jsx
+++ src/index.jsx
@@ -9,12 +9,10 @@ import { intelligenceInc } from './actions';
 
 require('./style.css');
 
-/* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-/* eslint-enable */
 
 render(
   <Provider store={store}>
